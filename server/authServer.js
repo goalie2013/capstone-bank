bea6fbef3448211d4b9f675582544b90e407d2d2
Gabriel Grinstein <gabriel.grinstein@gmail.com>
2022-11-02 00:53:57 -0700

    change port env var

--- server/authServer.js
+++ server/authServer.js
@@ -3,8 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 // const middleware = require("./middleware/auth");
-const port = process.env.SERVER_PORT;
-console.log("port", port);
+// const port = process.env.SERVER_PORT;
+// console.log("port", port);
 
 const app = express();
 // CORS FOR DEVELOPMENT ONLY
@@ -15,13 +15,13 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // Create & Send JWT Token on Login
-app.post("/login", (req, res) => {
-  // Authenticated user
-  const user = req.body.user;
+// app.post("/login", (req, res) => {
+//   // Authenticated user
+//   const user = req.body.user;
 
-  const jwtToken = jwt.sign(user, process.env.TOKEN_SECRET);
-  res.json({ token: jwtToken });
-});
+//   const jwtToken = jwt.sign(user, process.env.TOKEN_SECRET);
+//   res.json({ token: jwtToken });
+// });
 
 // Get JWT, Verify it, and return user
 function authenticateToken(req, res, next) {
