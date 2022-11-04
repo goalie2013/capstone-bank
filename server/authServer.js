aa849a438e91571e6cff48abcff37f8077e35462
Gabriel Grinstein <gabriel.grinstein@gmail.com>
2022-11-03 21:40:27 -0700

    change no jwt token in verifyTokenExists to error code 401

--- server/authServer.js
+++ server/authServer.js
@@ -52,7 +52,7 @@ function verifyTokenExists(req, res, next) {
   const jwtToken = authHeader && authHeader.split(" ")[1];
   console.log("jwtToken", jwtToken, typeof jwtToken);
 
-  if (jwtToken == null) {
+  if (jwtToken == "null") {
     console.log("verifyTokenExists Should return 401");
     res.sendStatus(401);
   }
