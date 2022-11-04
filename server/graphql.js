124611c70a58810a76b5cfe02f6163e7013122c7
Gabriel Grinstein <gabriel.grinstein@gmail.com>
2022-11-04 03:08:34 -0700

    change to puppeteer-core

--- server/graphql.js
+++ server/graphql.js
@@ -107,7 +107,24 @@ app.post("/newtoken", async (req, res) => {
 });
 
 app.get("/screenshot", (req, res) => {
-  takeScreenshot();
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`https://betterbankingapp.net/deposit/${userId}`);
+    await page.emulateMediaType("screen");
+    const pdf = await page.pdf({
+      path: `${date}.pdf`,
+      format: "Letter",
+      margin: {
+        top: "20px",
+        bottom: "40px",
+        left: "20px",
+        right: "20px",
+      },
+    });
+    res.send(pdf);
+    await browser.close();
+  })();
 });
 
 app.use(
