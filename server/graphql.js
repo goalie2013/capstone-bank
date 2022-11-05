1d3595237aa8ab4b9de90a196dff6cff3cc2bb6f
Gabriel Grinstein <gabriel.grinstein@gmail.com>
2022-11-05 01:42:18 -0700

    puppeteer refactor

--- server/graphql.js
+++ server/graphql.js
@@ -96,12 +96,19 @@ app.post("/newtoken", async (req, res) => {
   const tokenList = tokenObj[0]["tokens"];
 
   console.log("/newtoken refreshToken", refreshToken);
-  console.log("/newtoken tokenList[-1]", tokenList[tokenList.length - 1]);
+  console.log();
+  console.log("/newtoken tokenList[-1]", tokenList[tokenList.length - 1], "\n");
 
   if (refreshToken == null) return res.sendStatus(401);
-  if (!tokenList.includes(refreshToken)) return res.sendStatus(403);
+  if (!tokenList.includes(refreshToken)) {
+    console.log("Refresh Token NOT in Token List");
+    return res.sendStatus(403);
+  }
   jwt.verify(refreshToken, process.env.REFRESH_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) {
+      console.error(err.message);
+      return res.sendStatus(403);
+    }
     console.log("/newtoken user: ", user);
     const accessToken = generateToken(
       { id: user.id, name: user.name, email: user.email },
@@ -113,38 +120,9 @@ app.post("/newtoken", async (req, res) => {
 });
 
 app.get("/screenshot", (req, res) => {
-  (async () => {
-    try {
-      const { id, date, type } = req.body;
-      const browser = await puppeteer.launch();
-      // {
-      //   executablePath: executablePath(),
-      // }
-      const page = await browser.newPage();
-      await page.goto(`https://betterbankingapp.net/deposit/${id}`);
-      await page.emulateMediaType("screen");
-      // await page.setViewport({
-      //   width: 1920,
-      //   height: 1280,
-      //   deviceScaleFactor: 1,
-      // });
-      const pdf = await page.pdf({
-        path: `${type} ${date}.pdf`,
-        format: "Letter",
-        margin: {
-          top: "20px",
-          bottom: "40px",
-          left: "20px",
-          right: "20px",
-        },
-      });
-      res.send(pdf);
-      await browser.close();
-    } catch (err) {
-      console.error("/screenshot ERROR", err.message);
-      res.sendStatus(503);
-    }
-  })();
+  const { id, date, type } = req.query;
+  console.log("req.query", req.query);
+  takeScreenshot(id, date, type, res);
 });
 
 app.use(
--- server/puppeteer.js
+++ server/puppeteer.js
@@ -1,24 +1,77 @@
-// const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
 
-function takeScreenshot(date) {
+const takeScreenshot = async (id, date, type, res) => {
   console.log("takeScreenshot FUNCTION");
-  (async () => {
-    const browser = await puppeteer.launch();
+  let browser;
+
+  // Create folder to save screenshots into
+  if (!fs.existsSync("screenshots")) fs.mkdirSync("screenshots");
+
+  //   (async () => {
+  const typeLower = type.toLowerCase();
+  try {
+    browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(`https://betterbankingapp.net/deposit/${userId}`);
+    await page.goto(`https://betterbankingapp.net/${typeLower}/${id}`);
     await page.emulateMediaType("screen");
-    const pdf = await page.pdf({
-      path: `${date}.pdf`,
-      format: "Letter",
-      margin: {
-        top: "20px",
-        bottom: "40px",
-        left: "20px",
-        right: "20px",
+    await page.setViewport({ width: 1440, height: 1080 });
+
+    // { path: `${type}-${date}.png` }
+    const image = await page.screenshot({
+      type: "jpeg",
+      quality: 100,
+      clip: {
+        x: 0,
+        y: 70,
+        width: 640,
+        height: 360,
       },
+      omitBackground: true,
     });
-    res.send(pdf);
+    const base64Image = Buffer.from(image).toString("base64");
+    console.log("image", image);
+    console.log("base64Image", base64Image);
+    res.writeHead(200, {
+      "Content-Type": "image/jpeg",
+      "Content-Length": image.length,
+    });
+
+    res.end(base64Image);
+  } catch (err) {
+    console.error("screenshot ERROR", err.message);
+    res.sendStatus(503);
+  } finally {
     await browser.close();
+  }
+  //   })();
+};
+
+function createPdf(id, date, type, res) {
+  console.log("createPdf FUNCTION");
+  (async () => {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      await page.goto(`https://betterbankingapp.net/deposit/${id}`);
+      await page.emulateMediaType("screen");
+      // await page.setViewport({ width: 1920,height: 1280 });
+      const pdf = await page.pdf({
+        path: `${type} ${date}.pdf`,
+        format: "Letter",
+        margin: {
+          top: "20px",
+          bottom: "40px",
+          left: "20px",
+          right: "20px",
+        },
+      });
+      res.send(pdf);
+      await browser.close();
+    } catch (err) {
+      console.error("/screenshot ERROR", err.message);
+      res.sendStatus(503);
+    }
   })();
 }
 module.exports = { takeScreenshot };
